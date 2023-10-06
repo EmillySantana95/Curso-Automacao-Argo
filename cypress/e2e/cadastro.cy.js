252f8a58edc9b16f0c808ac11cae0c2563a9d329
Emilly Santana <emillyandradesantana@hotmail.com>
2023-10-06 10:43:00 -0300

    Automacao Cypress

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,60 +1,95 @@
 /// <reference types="cypress" />
-const faker = require('faker-br')
+import Home from "../support/pages/home/index"
+import Deliver from "../support/pages/deliver/index"
+import Utilidades from "../support/pages/Utilidades"
+
+
 describe('Cadastro de usuários', () => {
+
+    beforeEach(() => {
+        Home.acessarHomeBugerEats(`${Cypress.env('urlBase')}`)
+        Home.acessarCadastro()
+    })
+
     it('Deve efetuar cadastro inserindo CNH com método de entrega Moto', () => {
 
-        //FakerCadastro
-        let fakerNomeCompleto = `${faker.name.firstName()} ${faker.name.lastName()}`
-        console.log('FakerName: ' + fakerNomeCompleto)
-        let fakerEmail = faker.internet.email()
-        let fakerCpf = faker.br.cpf()
-        const dddsValido = ['11', '21', '31', '41', '48', '61']
-        const dddEstado = faker.random.arrayElement(dddsValido)
-        const numeroTelefone = faker.random.number({ min: 10000000, max: 99999999 })
-        const fakerTelefone = `(${dddEstado})9${numeroTelefone}`
+        const dadosUsuario = Utilidades.gerarDadosFakerUsuario()
+        const dadosEndereco = Utilidades.gerarDadosFakerEndereco()
 
-        //FakerEndereço
-        const fakerCep = faker.address.zipCodeValidByState()
-        const fakerNumero = faker.address.streetAddress().match(/\d+/g).join('')
-        const fakerComplemento = `Apto: ${faker.random.number({ min: 1, max: 300 })} Bloco:${faker.random.number({ min: 1, max: 3 })}`
-
-        //Acessa Publicação Buger-eats e valida que estamos na home
-        cy.visit('https://buger-eats.vercel.app/')
-        cy.get('main > h1').should('be.visible').and('have.text', 'Seja um parceiro entregador pela Buger Eats')
-
-        //Acessa a tela de cadastro
-        cy.get('strong').should('be.visible').and('have.text', 'Cadastre-se para fazer entregas').click()
-        cy.get('form > h1').should('be.visible').and('contain.text', 'Cadastre-se para')
-
-        //Preenche os dados do usuario
-        cy.get('input[placeholder="Nome completo"]').should('be.visible').type(fakerNomeCompleto)
-        cy.get('input[placeholder="CPF somente números"]').should('be.visible').type(fakerCpf)
-        cy.get('input[name="email"]').should('be.visible').type(fakerEmail)
-        cy.get('input[placeholder="Whatsapp"]').should('be.visible').type(fakerTelefone)
-
-        //Preenche o endereço
-        cy.get('input[placeholder="CEP"]').should('be.visible').type('08673000')
-        cy.get('input[type="button"]').should('be.visible').click()
-        cy.get('input[placeholder="Número"]').should('be.visible').type(fakerNumero)
-        cy.get('input[name="address-details"]').should('be.visible').type(fakerComplemento)
-
-        //Selecionar método de entrega
-        cy.get('li > span').eq(0).should('be.visible').and('have.text', 'Moto').click()
-        cy.get('li[class="selected"').eq(0).should('be.visible')
-
-        //Upload imagem CNH
-        cy.get('.dropzone > p').should('be.visible').and('have.text', 'Foto da sua CNH')
-        cy.get('input[type="file"]').invoke('show').selectFile('cypress/support/imagem/cnh.png').then(($input) => {
-            console.log($input)
-            const files = $input[0].files
-            expect(files[0].name).to.eq('cnh.png')
-            cy.get('.button-success').should('be.visible').and('have.text', 'Cadastre-se para fazer entregas').click()
+        Deliver.preencherDadosDoUsuario({
+            nomeCompleto: dadosUsuario.nomeCompleto,
+            cpf: dadosUsuario.cpf,
+            email: dadosUsuario.email,
+            telefone: dadosUsuario.telefone
         })
 
-        //Asserção
-        cy.get('#swal2-title').should('be.visible').and('have.text', 'Aí Sim...')
-        cy.get('button[class="swal2-confirm swal2-styled"]').should('be.visible').click()
-        cy.get('main > h1').should('be.visible').and('have.text', 'Seja um parceiro entregador pela Buger Eats')
+        Deliver.preencherEnderecoUsuario({
+            cep: dadosEndereco.cep,
+            numero: dadosEndereco.numero,
+            complemento: dadosEndereco.complemento
+        })
 
-    });
-});
+        Deliver.selecionarMetodoEntrega('Moto')
+
+        Deliver.efetuarUploadCnh({
+            caminhoArquivo: 'cypress/support/imagem/cnh.jpg',
+            nomeArquivo: 'cnh.jpg'
+        })
+
+        Deliver.validarCadastroSucesso()
+    })
+    it('Deve efetuar cadastro inserindo CNH com método de entrega Bicicleta', () => {
+
+        const dadosUsuario = Utilidades.gerarDadosFakerUsuario()
+        const dadosEndereco = Utilidades.gerarDadosFakerEndereco()
+
+        Deliver.preencherDadosDoUsuario({
+            nomeCompleto: dadosUsuario.nomeCompleto,
+            cpf: dadosUsuario.cpf,
+            email: dadosUsuario.email,
+            telefone: dadosUsuario.telefone
+        })
+
+        Deliver.preencherEnderecoUsuario({
+            cep: dadosEndereco.cep,
+            numero: dadosEndereco.numero,
+            complemento: dadosEndereco.complemento
+        })
+
+        Deliver.selecionarMetodoEntrega('Bicicleta')
+
+        Deliver.efetuarUploadCnh({
+            caminhoArquivo: 'cypress/support/imagem/cnh.jpg',
+            nomeArquivo: 'cnh.jpg'
+        })
+
+        Deliver.validarCadastroSucesso()
+    })
+    it('Deve efetuar cadastro inserindo CNH com método de entrega Van/Carro', () => {
+
+        const dadosUsuario = Utilidades.gerarDadosFakerUsuario()
+        const dadosEndereco = Utilidades.gerarDadosFakerEndereco()
+
+        Deliver.preencherDadosDoUsuario({
+            nomeCompleto: dadosUsuario.nomeCompleto,
+            cpf: dadosUsuario.cpf,
+            email: dadosUsuario.email,
+            telefone: dadosUsuario.telefone
+        })
+
+        Deliver.preencherEnderecoUsuario({
+            cep: dadosEndereco.cep,
+            numero: dadosEndereco.numero,
+            complemento: dadosEndereco.complemento
+        })
+
+        Deliver.selecionarMetodoEntrega('Van/Carro')
+
+        Deliver.efetuarUploadCnh({
+            caminhoArquivo: 'cypress/support/imagem/cnh.jpg',
+            nomeArquivo: 'cnh.jpg'
+        })
+
+        Deliver.validarCadastroSucesso()
+    })
+})
